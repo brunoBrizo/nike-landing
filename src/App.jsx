f2d3dbe8f33d939529357f7b9ffb03b0b6e3d4e9
Bruno Brizolara <bbrizolara7@gmail.com>
2024-06-28 19:56:13 -0300

    add new arrivals section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Nav } from "./components/Nav";
+import { NewArrivalsSection } from "./components/NewArrivalsSection";
 import { ShoeDetail } from "./components/ShoeDetail";
+import { SHOE_LIST } from "./constant";
 
 export function App() {
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
       <Nav />
       <ShoeDetail />
+      <NewArrivalsSection items={SHOE_LIST} />
     </div>
   );
 }
