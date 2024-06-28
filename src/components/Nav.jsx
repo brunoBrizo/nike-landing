47b7bd10ae7749dbd05b09ec39758bed17e93aa4
Bruno Brizolara <bbrizolara7@gmail.com>
2024-06-27 22:02:35 -0300

    add custom animations

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,9 +3,16 @@ import NikeLogo from "../assets/nike-logo.svg?react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { useState } from "react";
 
-const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"];
+const ROUTES = [
+  "Home",
+  "About",
+  "Services",
+  "Pricing",
+  "Contact",
+];
 export function Nav() {
-  const [isMobileMenuShown, setIsMobileMenuShown] = useState(false);
+  const [isMobileMenuShown, setIsMobileMenuShown] =
+    useState(false);
   return (
     <nav className="relative z-10 flex flex-wrap items-center justify-between">
       {/* Logo */}
@@ -46,7 +53,7 @@ export function Nav() {
       </div>
 
       {/* Cart button */}
-      <div className="fixed bottom-4 left-4 lg:static lg:mr-8">
+      <div className="btn-press-anim fixed bottom-4 left-4 lg:static lg:mr-8">
         <div className="flex-center h-12 w-12 cursor-pointer rounded-full bg-white shadow-md">
           <TbShoppingBag />
         </div>
