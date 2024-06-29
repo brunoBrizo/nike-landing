100899a545e67e92cd7f73c15ccb5fd0cb60981f
Bruno Brizolara <bbrizolara7@gmail.com>
2024-06-28 21:29:55 -0300

    add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Nav } from "./components/Nav";
 import { NewArrivalsSection } from "./components/NewArrivalsSection";
 import { ShoeDetail } from "./components/ShoeDetail";
 import { Sidebar } from "./components/Sidebar";
 import { SHOE_LIST } from "./constant";
 import { Cart } from "./components/Cart";
-
+import { BiMoon, BiSun } from "react-icons/bi";
 const FAKE_CART_ITEMS = SHOE_LIST.map((shoe) => {
   return {
     product: shoe,
@@ -16,8 +16,22 @@ const FAKE_CART_ITEMS = SHOE_LIST.map((shoe) => {
 
 export function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem("isDarkMode");
+    if (isDarkMode === "true") {
+      window.document.documentElement.classList.add("dark");
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    window.document.documentElement.classList.toggle("dark");
+    localStorage.setItem(
+      "isDarkMode",
+      window.document.documentElement.classList.contains("dark"),
+    );
+  };
   return (
-    <div className="animate-fadeIn p-10 xl:px-24">
+    <div className="dark:bg-night animate-fadeIn p-10 xl:px-24">
       <Nav onClickShoppingBtn={() => setIsSidebarOpen(true)} />
       <ShoeDetail />
       <NewArrivalsSection items={SHOE_LIST} />
@@ -27,6 +41,15 @@ export function App() {
       >
         <Cart cartItems={FAKE_CART_ITEMS} />
       </Sidebar>
+      <div className=" fixed bottom-4 right-4">
+        <button
+          onClick={toggleDarkMode}
+          className="bg-night-50 dark:text-night rounded-full px-4 py-2 text-white shadow-lg dark:bg-white"
+        >
+          <BiSun className="hidden dark:block" />
+          <BiMoon className="dark:hidden" />
+        </button>
+      </div>
     </div>
   );
 }
